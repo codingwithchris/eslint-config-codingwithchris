582628480f15563d0b0458cdd1574af8c4dcfa64
Chris Hahn <chris+digital@chrishahn.me>
2020-09-24 14:26:08 -0400

    Define Required TSConfig Parser Options + Bump to 1.0.1

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,4 +1,7 @@
 module.exports = {
     extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
     plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
 };
