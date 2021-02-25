644f4ad3da30484ca8fa232ff412b5dcb02492ef
Chris Hahn <chris+digital@chrishahn.me>
2021-02-25 08:44:03 -0500

    Update Dependencies

--- rules/typescript.js
+++ rules/typescript.js
@@ -9,5 +9,6 @@ module.exports = {
     },
     rules: {
         '@typescript-eslint/no-use-before-define': ['warn'], // fixes https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined/64024916#64024916
+        '@typescript-eslint/no-empty-interface': ['warn'], // warn about this but don't throw an error because we sometimes use empty interfaces as placeholders while scaffolding out functionality
     },
 };
